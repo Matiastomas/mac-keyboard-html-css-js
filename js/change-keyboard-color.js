f93c44a60d7ef6137f752e7b1eb3d7a9807ed78a
Matias Tomás <matiasgracianotomas@gmail.com>
2021-07-11 22:19:31 +0200

    Added function to set space grey
    - Added a function to   set Keyboard backgroundcolor to space grey

--- js/change-keyboard-color.js
+++ js/change-keyboard-color.js
@@ -24,6 +24,18 @@ function setKeyboardColorToSilver(){
 
 }
 /*
+The following function change the
+ keyboard color to silver once the user 
+ click on the silver (radio) option, which 
+ is the default color
+ */
+ function setKeyboardColorToSpaceGrey(){
+  document.getElementById("main").style.backgroundColor ="#f9f9f9";
+  document.getElementById("grid-for-keys").style.backgroundColor="#f9f9f9";
+  document.getElementById("grid-for-keys").style.borderColor ="#f9f9f9";
+  document.getElementById("keyboard-pad").style.backgroundColor ="#f9f9f9";
+}
+/*
   //Execute the function setKeyboardColorSilver once the user click on the silver (radio) option
       document.getElementById("silver").onclick = function () {
           setKeyboardColorToSilver();
