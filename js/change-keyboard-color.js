597f80dd9bad164863071e165e15262b2b9139dc
Matias Tomás <matiasgracianotomas@gmail.com>
2021-07-11 15:12:44 +0200

    Added two functions to change keyboard background color
    - Added a function to set the keyboard background color to gold
    
    - Added a second function to set the keyboard background color to silver

---
+++ js/change-keyboard-color.js
@@ -0,0 +1,21 @@
+/*
+
+The following function change the
+ keyboard color to gold once the user 
+ click on the Gold (radio) option
+
+ */
+function setKeyboardColorToGold(){
+
+}
+/*
+
+The following function change the
+ keyboard color to silver once the user 
+ click on the silver (radio) option, which 
+ is the default color
+
+ */
+function setKeyboardColorToSilver(){
+    
+}
