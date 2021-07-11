ccea6693d75ba3a6eea3d471eef1c6fa6365a3a6
Matias Tomás <matiasgracianotomas@gmail.com>
2021-07-11 17:12:50 +0200

    Updated change-keyboard-color.js
    - Space and identation

--- js/change-keyboard-color.js
+++ js/change-keyboard-color.js
@@ -1,26 +1,21 @@
 /*
-
 The following function change the
  keyboard color to gold once the user 
  click on the Gold (radio) option
-
- */
+*/
 function setKeyboardColorToGold(){
   document.getElementsByClassName('grid-for-keys').style.backgroundColor ="#edd290";
 
 }
 /*
-
 The following function change the
  keyboard color to silver once the user 
  click on the silver (radio) option, which 
  is the default color
-
  */
 function setKeyboardColorToSilver(){
   document.getElementsByClassName('grid-for-keys').style.backgroundColor ="#c0c0c0";
 }
-
 //Execute the function setKeyboardColorToGold once the user click on the gold (radio) option
 document.getElementById('gold').onclick = function () {
     setKeyboardColorToGold();
